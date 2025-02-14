4c0db1fb00e368bb71fb5c7e45b660fa8e395027
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-02-14 19:15:11 +0100

    typescript

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let id = 5;
+console.log("ID", id);
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let channel: string = "simple arab code";
+let isSuccess: boolean = true;
+let id: number = 5;
+let x: any = 5;
+x = true;
+let ids: number[] = [1, 2, 3, 4];
+let strArr: string[] = ["hehe", "lol"];
+let arr: (string | number | boolean)[] = [1, true, "str"];
+let employee: (string | number)[][] = [
+  [1, "number"],
+  [2, "hehe"],
+  [4, "numhhber"],
+];
