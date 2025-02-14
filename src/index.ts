0ab4d3d9eb048949a08cd9be99c2029f03d11e35
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-02-14 19:15:55 +0100

    typeshit

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ let x: any = 5;
 x = true;
 let ids: number[] = [1, 2, 3, 4];
 let strArr: string[] = ["hehe", "lol"];
+
 let arr: (string | number | boolean)[] = [1, true, "str"];
 let employee: (string | number)[][] = [
   [1, "number"],
